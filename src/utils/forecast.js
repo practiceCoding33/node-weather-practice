4524247255951fb52e4b3343371234b0977cf85a
Taylor Prescott <practiceCoding33@gmail.com>
2020-10-15 14:05:41 -0400

    Init Commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,23 @@
+const request = require('postman-request');
+
+const forecast = (lat, long, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=fbe12ceeb68d85d5b1386e31f70cb5cf&query=' + lat +',' + long + '&units=f';
+
+    request({url, json: true}, (error, { body }) => {
+
+        if (error) {
+            callback('Unable to connect to location services.', undefined);
+        } else if (body.error) {
+            callback('Unable to find location. Try another search.', undefined);
+        } else {
+            callback(undefined, {
+                temperature: body.current.temperature,
+                feelsLike: body.current.feelslike,
+                description: body.current.weather_descriptions[0]
+            });
+        }
+
+    });
+};
+
+module.exports = forecast;
